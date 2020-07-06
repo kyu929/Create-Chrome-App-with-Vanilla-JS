82481e57553b0a383b4314d805aad60218434228
kyu929 <j2mity@gmail.com>
2020-07-06 23:51:36 +0900

    Add Weather API_KEY and function

--- weather.js
+++ weather.js
@@ -1,4 +1,19 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "95d4f8e955f162ace268c791195ca08d"
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +27,7 @@ function handleGeoSucces(position) {
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,7 +43,8 @@ function loadCoords() {
     if(loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
